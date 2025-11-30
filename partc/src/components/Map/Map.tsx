7f6ba3521a82ed72a5107437ecd21c73b89a90b2
Anthony Sokolovsky <tony9sokol@gmail.com>
2025-11-30 11:57:46 +0200

    issue 2

--- partc/src/components/Map/Map.tsx
+++ partc/src/components/Map/Map.tsx
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from "react";
 import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
+import * as utm from "utm";
 
 export function Map() {
-  const center: [number, number] = [39.8283, -98.5795];
+  const center: [number, number] = [32.0853, 34.7818];
 
-  // State to track marker positions
   const [markerPositions, setMarkerPositions] = useState([
-    [39.8283, -98.5795],
-    [40.0, -97.0],
+    [32.0853, 34.7818], // Tel Aviv
+    [32.794, 34.9896], // Haifa
   ]);
 
   useEffect(() => {
-    // Initialize the map
     const map = L.map("map").setView(center, 4);
 
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
@@ -21,12 +20,19 @@ export function Map() {
         '&copy; <a href="https://www.openstreetmap.org/copyright">OSM</a>',
     }).addTo(map);
 
-    // Create markers and make them draggable
     const markers = markerPositions.map((pos, index) => {
       const marker = L.marker(pos, { draggable: true }).addTo(map);
-      marker.bindPopup(`Marker ${index + 1}`);
+      const utmCoords = utm.fromLatLon(pos[0], pos[1]);
 
-      // Update state when marker is dragged
+      marker.bindPopup(
+        `Marker ${index + 1}<br>Lat: ${pos[0].toFixed(
+          4
+        )}, Lng: ${pos[1].toFixed(4)} , UTM: Zone ${utmCoords.zoneNum}${
+          utmCoords.zoneLetter
+        }, Easting: ${utmCoords.easting.toFixed(
+          2
+        )}, Northing: ${utmCoords.northing.toFixed(2)}`
+      );
       marker.on("dragend", () => {
         const newPos = marker.getLatLng();
         setMarkerPositions((prev) => {
@@ -34,6 +40,17 @@ export function Map() {
           updated[index] = [newPos.lat, newPos.lng];
           return updated;
         });
+        const newUtm = utm.fromLatLon(newPos.lat, newPos.lng);
+
+        marker.setPopupContent(
+          `Marker ${index + 1}<br>
+           Lat: ${newPos.lat.toFixed(4)}, Lng: ${newPos.lng.toFixed(4)}<br>
+           UTM: Zone ${newUtm.zoneNum}${
+            newUtm.zoneLetter
+          }, Easting: ${newUtm.easting.toFixed(
+            2
+          )}, Northing: ${newUtm.northing.toFixed(2)}`
+        );
       });
 
       return marker;
@@ -42,7 +59,6 @@ export function Map() {
     return () => {
       map.remove();
     };
-  }, []); // empty deps so map initializes only once
-
+  }, []);
   return <div id="map" style={{ height: "100vh", width: "100%" }} />;
 }
