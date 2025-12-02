704f95805457a52e42f543d57bf64aba01e2fab2
Anthony Sokolovsky <tony9sokol@gmail.com>
2025-12-02 14:21:01 +0200

    fix comments

--- partc/server/main.py
+++ partc/server/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import uvicorn
+
 
 app = FastAPI(title="Basic FastAPI Server")
 
@@ -7,5 +9,4 @@ def read_root():
     return {"message": "Hello, FastAPI!"}
 
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
